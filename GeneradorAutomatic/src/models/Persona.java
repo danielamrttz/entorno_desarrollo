995bac4f765779fc5fa7f7b5840f2c0c49d49cf2
danielamrttz <danielamartinezfernandez10@gmail.com>
2024-03-14 16:11:16 +0100

    Proyecto personas creadas

---
+++ GeneradorAutomatic/src/models/Persona.java
@@ -0,0 +1,102 @@
+package models;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido;
+
+    /**
+     * 
+     */
+    private int edad;
+
+
+    public Persona(String dni, String nombre, String apellidos, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+   
+    /**
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param apellidos:String
+     */
+    public void setApellido(String apellido) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getApellidos() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param edad:int
+     */
+    public void setEdad(int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+}
